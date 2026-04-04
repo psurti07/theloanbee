e11ad105b4ebaec4ee6a8ba48098dea3fe2bb8b5
vw-uday <verloop.dev4@gmail.com>
2026-04-04 18:28:00 +0530

    remove google code

--- resources/views/layouts/selfapply.blade.php
+++ resources/views/layouts/selfapply.blade.php
@@ -2,15 +2,6 @@
 <html lang="en">
 @include('partials.selfapply.head')
 <body>
-    <!-- PRELOADER SPINNER -->	
-    <div id="loading" class="loading--theme">
-        <div id="loading-center"><span class="loader-img"></span></div>
-    </div>
-    
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TN4W7WVS"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
 <!-- PAGE CONTENT -->
 <div id="page" class="page font--poppins">
